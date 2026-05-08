7cd8d84efb17586b8ae9e7a69d2a7b18019a806d
Sam61410 <Samuelporier@sachristian.org>
2026-05-08 14:24:08 -0500

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,40 @@
+using StarterAssets;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] GameObject projectileHitVFX;
+    [SerializeField] GameObject camera;
+
+    Rigidbody rb;
+
+    int damage;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+    }
+    public void Update()
+    {
+        rb.linearVelocity = camera.transform.forward * speed;
+        
+    }
+    public void Init(int damage)
+    {
+        this.damage = damage;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+        playerHealth?.TakeDamage(damage);
+
+        Instantiate(projectileHitVFX, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
